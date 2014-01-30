c0b6f3d52652225e635e736668158bb2e4cab167
Dennis Park <dennisjoonpark@gmail.com>
2014-01-29 19:58:14 -0500

    edited spring class

---
+++ src/masses/Mass.java
@@ -0,0 +1,5 @@
+package masses;
+
+public class Mass {
+
+}
---
+++ src/springs/Spring.java
@@ -0,0 +1,12 @@
+package springs;
+
+import masses.Mass;
+
+public class Spring {
+	public Spring(Mass a, Mass b) {
+		
+	}
+	public Spring() {
+		
+	}
+}
