6b1b9627d7e1e946158d7e5db30a0b2d899555c2
Dennis Park <dennisjoonpark@gmail.com>
2014-02-03 02:26:10 -0500

    rectangle to object for spring, to revert later

--- src/springs/Spring.java
+++ src/springs/Spring.java
@@ -1,15 +1,24 @@
 package springs;
 
+import org.jbox2d.collision.PolygonDef;
 import org.jbox2d.common.Vec2;
+
 import jboxGlue.PhysicalObject;
 import jboxGlue.PhysicalObjectRect;
 import jgame.JGColor;
 import masses.Mass;
 
-public class Spring extends PhysicalObjectRect{
-	private Mass mStart, mEnd;
-	private double mLength, mKval;
-	private int mStartX, mStartY, mEndX, mEndY;
+public class Spring extends PhysicalObject{
+	private Mass mStart;
+	private Mass mEnd;
+	private double mLength;
+	private double mKval;
+	private int mStartX;
+	private int mStartY;
+	private int mEndX;
+	private int mEndY;
+	private double mWidth;
+	private double mHeight;
 
 	private static final int DEFAULT_KVAL = 1;
 	private static JGColor DEFAULT_COLOR = JGColor.blue;
@@ -18,11 +27,22 @@ public class Spring extends PhysicalObjectRect{
 	 * Spring constructor that sets private member attributes
 	 */
 	public Spring(Mass m1, Mass m2, double length, double k) {
-		super("spring", 1, DEFAULT_COLOR, pfwidth, pfheight);
+		super("spring", 1, DEFAULT_COLOR);
 		mStart = m1;
 		mEnd = m2;
 		mLength = length;
 		mKval = k;
+		
+	}
+	private void init(double width, double height, double mass) {
+		mWidth = width;
+		mHeight = height;
+		// make it a rect
+		PolygonDef shape = new PolygonDef();
+		shape.density = (float) mass;
+		shape.setAsBox((float) width, (float) height);
+		createBody(shape);
+		setBBox(-(int) width / 2, -(int) height / 2, (int) width, (int) height);
 	}
 
 	public Spring(Mass start, Mass end) {
@@ -34,8 +54,8 @@ public class Spring extends PhysicalObjectRect{
 	}
 
 	private static double computeLength(Mass m1, Mass m2) {
-		return Math.sqrt(Math.pow(m1.getX() - m2.getX(), 2)
-				+ Math.pow(m1.getY() - m2.getY(), 2));
+		return Math.sqrt(Math.pow(m1.x - m2.x, 2)
+				+ Math.pow(m1.y - m2.y, 2));
 	}
 
 	protected double getLength() {
@@ -55,19 +75,17 @@ public class Spring extends PhysicalObjectRect{
 		// either the myFirst or mySecond coordinates should be equivalent and
 		// so have no effect
 		Vec2 force = new Vec2(
-				(float)((x-mStart.getX())+(x-mEnd.getX())),
-				(float)((y-mStart.getY())+(y-mEnd.getY()))
+				(float)((x-mStart.x)+(x-mEnd.x)),
+				(float)((y-mStart.y)+(y-mEnd.y))
 				);
 		force.normalize();
 		force = force.mul(mag);
 		return force;
 	}
-
-	@Override
-	protected void paintShape() {
-		myEngine.setColor(myColor);
-		myEngine.drawLine(mStartX, mEndY, mStartX, mEndY);
+	public void updateString() {
+		//TODO
 	}
+	
 	
 	public Mass getStart() {
 		return mStart;
@@ -86,5 +104,10 @@ public class Spring extends PhysicalObjectRect{
 	}
 	public int getEndY() {
 		return mEndY;
+	}
+	@Override
+	protected void paintShape() {
+		// TODO Auto-generated method stub
+		
 	}
 }
