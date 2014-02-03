8193eed732206b96d041d53213a0c38681495028
Dennis Park <dennisjoonpark@gmail.com>
2014-02-03 05:42:59 -0500

    changes?

--- src/springies/EnvironmentForces.java
+++ src/springies/EnvironmentForces.java
@@ -1,8 +1,6 @@
 package springies;
+import jboxGlue.WorldManager;
 
-import jboxGlue.PhysicalObject;
-import jboxGlue.PhysicalObjectRect;
-import jgame.JGColor;
 import org.jbox2d.common.Vec2;
 
 
@@ -11,20 +9,36 @@ public class EnvironmentForces {
     private Vec2 mViscosity;
     private Vec2 mCenterOfMass;
     private Vec2 mWallRepulsion;
+    private Springies springies;
 
-    public EnvironmentForces (Vec2 gravity, Vec2 viscosity, Vec2 center_of_mass, Vec2 wall_repulsion) {
-
+    double mGravMag;
+    double mViscMag;
+    double mCOMMag;
+    double[] mWallMag = new double[4];
+    double[] mWallExp = new double[4];
+    
+    public EnvironmentForces() {
+    	readXML();
     }
 
-    public EnvironmentForces (String xml_fn) {
+    private void readXML(String xml_fn) {
+        EnvironmentXMLReader envreader = new EnvironmentXMLReader(xml_fn);
         
     }
     
-    
+    public void doForces() {
+    	WorldManager.getWorld().
+    }
 
-    public Vec2 getGravity () {
-        if (mGravity == null) { return new Vec2(0.0f, 0.0f); }
-        return mGravity;
+    
+    
+    public Vec2 Gravity () {
+        if (mGravity != null) {
+        	WorldManager.getWorld().setGravity(
+        			new Vec2(0.0f, 0.0f)
+        			);
+        }
+        return new Vec2(0.0f, 0.0f);
     }
 
     public Vec2 getViscosity () {
