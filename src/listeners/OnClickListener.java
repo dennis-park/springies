bbb77f0051f0e23c638025f33488cd95c84eab13
Dennis Park <dennisjoonpark@gmail.com>
2014-02-11 03:27:10 -0500

    added listener parent classes

---
+++ src/listeners/OnClickListener.java
@@ -0,0 +1,6 @@
+package listeners;
+
+public interface OnClickListener {
+    public void doMouseEvent (boolean left_pressed, boolean right_pressed, int mouse_x, int mouse_y);
+    
+}
---
+++ src/listeners/OnKeyListener.java
@@ -0,0 +1,7 @@
+package listeners;
+
+public interface OnKeyListener {
+	
+	public void doKeyEvent (int key);
+	
+}
