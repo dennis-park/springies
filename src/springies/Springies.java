b58b3c1b3b0d94d61549994d3a905d9f6558b54f
Dennis Park <dennisjoonpark@gmail.com>
2014-02-11 01:52:31 -0500

    added conditional for per-frame actions

--- src/springies/Springies.java
+++ src/springies/Springies.java
@@ -17,145 +17,148 @@ import Parsers.XMLParserCaller;
 
 @SuppressWarnings("serial")
 public class Springies extends JGEngine {
-    public ArrayList<Assembly> mAssemblyList;
-    private Wall[] mWallArray;
-    private EnvironmentManager mEnvironmentManager;
-    private JGameActionListener mActionListener;
+	public ArrayList<Assembly> mAssemblyList;
+	private Wall[] mWallArray;
+	private EnvironmentManager mEnvironmentManager;
+	private JGameActionListener mActionListener;
 
-    private final int FPS = 40;
-    private final int FRAME_SKIP = 2;
-    
-    public Springies () {
-        // set the window size
-        int height = 480;
-        double aspect = 16.0 / 9.0;
-        initEngineComponent((int) (height * aspect), height);
-        mAssemblyList = new ArrayList<Assembly>();
-    }
+	private final int FPS = 40;
+	private final int FRAME_SKIP = 2;
 
-    @Override
-    public void initCanvas ()
-    {
-        setCanvasSettings(1, // width of the canvas in tiles
-                          1, // height of the canvas in tiles
-                          displayWidth(), // width of one tile
-                          displayHeight(), // height of one tile
-                          null,// foreground colour -> use default colour white
-                          null,// background colour -> use default colour black
-                          null); // standard font -> use default font
-    }
+	public Springies () {
+		// set the window size
+		int height = 480;
+		double aspect = 16.0 / 9.0;
+		initEngineComponent((int) (height * aspect), height);
+		mAssemblyList = new ArrayList<Assembly>();
+	}
 
-    @Override
-    public void initGame () {
-        setFrameRate(FPS, FRAME_SKIP);
-        WorldManager.initWorld(this);
-        mAssemblyList = new ArrayList<Assembly>();
-        mEnvironmentManager = new EnvironmentManager(this);
-        // mForceManager = new EnvironmentManager(this, environment_filename);
-        mActionListener = new JGameActionListener(this, mEnvironmentManager);
-    }
+	@Override
+	public void initCanvas ()
+	{
+		setCanvasSettings(1, // width of the canvas in tiles
+				1, // height of the canvas in tiles
+				displayWidth(), // width of one tile
+				displayHeight(), // height of one tile
+				null,// foreground colour -> use default colour white
+				null,// background colour -> use default colour black
+				null); // standard font -> use default font
+	}
 
-    private void testAssembly() {
-        String model_filename = "assets/lamp.xml";
-        String model_filename2 = "assets/daintywalker.xml";
-        makeModelFromXML(model_filename);
-        try {
-            Thread.sleep(1000);
-        }
-        catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        makeModelFromXML(model_filename2);
-    }
+	@Override
+	public void initGame () {
+		setFrameRate(FPS, FRAME_SKIP);
+		WorldManager.initWorld(this);
+		mAssemblyList = new ArrayList<Assembly>();
+		makeAssembly();
+		mEnvironmentManager = new EnvironmentManager(this);
+		// mForceManager = new EnvironmentManager(this, environment_filename);
+		mActionListener = new JGameActionListener(this, mEnvironmentManager);
+	}
 
-    private void makeModelFromXML (String filename) {
-        XMLParserCaller caller = new XMLParserCaller();
-        ModelParser parser = new ModelParser(this);
-        try {
-            caller.call(filename, parser);
-        }
-        catch (Exception e) {
-            System.out.println("Error: Unable to parse XML file");
-            e.printStackTrace();
-            System.exit(1);
-        }
-    }
+	private void testAssembly() {
+		String model_filename = "assets/lamp.xml";
+		String model_filename2 = "assets/daintywalker.xml";
+		makeModelFromXML(model_filename);
+		try {
+			Thread.sleep(1000);
+		}
+		catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		makeModelFromXML(model_filename2);
+	}
 
-    @Override
-    /**
-     * In each frame, Springies will check all the user input events and perform actions accordingly.
-     * It will also iterate through all the masses and apply the Forces acting upon the masses at the 
-     * moment. Then the JBox world will take 1 time step and update JGame accordingly.  
-     * 
-     */
-    public void doFrame ()
-    {
-        doListenerEvents();
-        // update game objects
-        mEnvironmentManager.doForces();
-        WorldManager.getWorld().step(1f, 1);
-        moveObjects();
-        checkCollision(1 + 2, 1);
-    }
+	private void makeModelFromXML (String filename) {
+		XMLParserCaller caller = new XMLParserCaller();
+		ModelParser parser = new ModelParser(this);
+		try {
+			caller.call(filename, parser);
+		}
+		catch (Exception e) {
+			System.out.println("Error: Unable to parse XML file");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 
-    // This is a helper method to call the built in JEngine listeners. This way
-    // we don't have to worry about coordinates, etc. This method will send the lastKeyPressed
-    // and the mouseEvents to the Listener class and the Listener class will perform
-    // the appropriate actions
-    private void doListenerEvents () {
-    	int last_key = getLastKey();
-    	this.clearLastKey();
-        mActionListener.doKeyEvent(last_key);
-        mActionListener
-                .doMouseEvent(getMouseButton(1), getMouseButton(3), getMouseX(), getMouseY());
-    }
+	@Override
+	/**
+	 * In each frame, Springies will check all the user input events and perform actions accordingly.
+	 * It will also iterate through all the masses and apply the Forces acting upon the masses at the 
+	 * moment. Then the JBox world will take 1 time step and update JGame accordingly.  
+	 * 
+	 */
+	public void doFrame ()
+	{
+		doListenerEvents();
+		// update game objects
+		if (!mAssemblyList.isEmpty()) {
+			mEnvironmentManager.doForces();
+			WorldManager.getWorld().step(1f, 1);
+			moveObjects();
+			checkCollision(1 + 2, 1);
+		}
+	}
 
-    public Wall[] getWalls () {
-        return mWallArray;
-    }
+	// This is a helper method to call the built in JEngine listeners. This way
+	// we don't have to worry about coordinates, etc. This method will send the lastKeyPressed
+	// and the mouseEvents to the Listener class and the Listener class will perform
+	// the appropriate actions
+	private void doListenerEvents () {
+		int last_key = getLastKey();
+		clearLastKey();
+		mActionListener.doKeyEvent(last_key);
+		mActionListener
+		.doMouseEvent(getMouseButton(1), getMouseButton(3), getMouseX(), getMouseY());
+	}
 
-    public void makeAssembly () {
-        JFileChooser chooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("XML documents", "xml");
-        chooser.setFileFilter(filter);
-        int returnVal = chooser.showDialog(null, "Load new Assembly file");
-        if (returnVal == JFileChooser.APPROVE_OPTION) {
-            File file = chooser.getSelectedFile();
-            this.loadAssemblyFromFile(file);
-        }
-    }
+	public Wall[] getWalls () {
+		return mWallArray;
+	}
 
-    public void loadAssemblyFromFile (File file) {
-        if (file != null) {
-            ModelParser factory = new ModelParser(this);
-            try {
-                makeModelFromXML(file.getAbsolutePath());
-                Assembly a = new Assembly();
-                for (Mass mass : factory.getMasses()) {
-                    a.add(mass);
-                }
-                for (Spring spring : factory.getSprings()) {
-                    a.add(spring);
-                }
-                mAssemblyList.add(a);
-                // a.addMuscles(factory.getAssemblyMuscles());
-            }
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
+	public void makeAssembly () {
+		JFileChooser chooser = new JFileChooser();
+		FileNameExtensionFilter filter = new FileNameExtensionFilter("XML documents", "xml");
+		chooser.setFileFilter(filter);
+		int returnVal = chooser.showDialog(null, "Load new Assembly file");
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = chooser.getSelectedFile();
+			loadAssemblyFromFile(file);
+		}
+	}
 
-    public List<Assembly> getAssemblyList () {
-        return mAssemblyList;
-    }
-    
-    public Assembly getAssembly(int index) {
-        return mAssemblyList.get(index);
-    }
+	public void loadAssemblyFromFile (File file) {
+		if (file != null) {
+			ModelParser factory = new ModelParser(this);
+			try {
+				makeModelFromXML(file.getAbsolutePath());
+				Assembly a = new Assembly();
+				for (Mass mass : factory.getMasses()) {
+					a.add(mass);
+				}
+				for (Spring spring : factory.getSprings()) {
+					a.add(spring);
+				}
+				mAssemblyList.add(a);
+				// a.addMuscles(factory.getAssemblyMuscles());
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public void clearLoadedAssemblies () {
-        mAssemblyList.clear();
-    }
+	public List<Assembly> getAssemblyList () {
+		return mAssemblyList;
+	}
+
+	public Assembly getAssembly(int index) {
+		return mAssemblyList.get(index);
+	}
+
+	public void clearLoadedAssemblies () {
+		mAssemblyList.clear();
+	}
 }
